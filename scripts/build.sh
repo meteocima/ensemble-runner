08b1eacf28cd38e541c80e288b342c833a4171d7
Andrea Parodi <andrea@parro.it>
2024-02-09 16:26:30 +0100

    posrproc build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ go build -o ./build/bin/prepvars ./cli/prepvars
 go build -o ./build/bin/ensrunner ./cli/ensrunner
 go build -o ./build/bin/dirprep ./cli/dirprep
 go build -o ./build/bin/hosts ./cli/hosts
+go build -o ./build/bin/postproc ./cli/postproc
 cp -v $TYPE.config.yaml ./build/config.yaml
 cp -rv templates/$TYPE ./build/templates
 cp -rv scripts ./build
