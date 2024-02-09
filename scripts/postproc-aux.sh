d01bc379e25d7393e087aa13d4738b9375206e9f
Andrea Parodi <andrea@parro.it>
2024-02-09 15:41:07 +0100

    postproc scripts

--- scripts/postproc-aux.sh
+++ scripts/postproc-aux.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-src=$1
-domain=$2
+mkdir -p results/aux
+regridded=results/aux/aux_regr_${INSTANT}.nc
 
-novar=../regridded_aux/novar_`basename $src`
-regridded=../regridded_aux/regr_`basename $src`
-mkdir -p `dirname $regridded`
+script="${ROOTDIR}/scripts/italy-cdo-d0${DOMAIN}-grid.txt"
 
-#ncks -O -x -v P_PL,C1H,C2H,C3H,C4H,C1F,C2F,C3F,C4F $src $novar
-cp $src $novar
-
-script="$WRFITA_ROOTDIR/scripts/grid_wrfita-d0$domain.txt"
-cdo_params="setreftime,2000-01-01,00:00:00 -setcalendar,standard -remapbil,$script -selgrid,1 "
-cdo $cdo_params $novar $regridded
-rm $novar
+cdo_params="setreftime,2000-01-01,00:00:00 -setcalendar,standard -remapbil,${script} -selgrid,1 "
+cdo ${cdo_params} ${SRC} ${regridded}
