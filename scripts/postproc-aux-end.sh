27ba260337c1868db718a5063dfaa8b7b2e14e52
Andrea Parodi <andrea@parro.it>
2024-02-22 14:52:44 +0100

    fixes

--- scripts/postproc-aux-end.sh
+++ scripts/postproc-aux-end.sh
@@ -3,6 +3,6 @@ set -e
 
 cd $SIM_WORKDIR/results/aux;
 
-cdo -O -v -z zip_4 mergetime aux-regr-d03-*.nc regr-d03-${START_FORECAST}.nc
-cdo -O -v -z zip_4 mergetime aux-regr-d01-*.nc regr-d01-${START_FORECAST}.nc
+cdo -O -v -f nc4c -z zip_4 mergetime aux-regr-d03-*.nc regr-d03-${START_FORECAST}.nc
+cdo -O -v -f nc4c -z zip_4 mergetime aux-regr-d01-*.nc regr-d01-${START_FORECAST}.nc
 
